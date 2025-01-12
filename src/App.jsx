2ec4d08b10e1845bbb560210e78dad5af32f5d71
Mavafelly <matthewfeliciano16@gmail.com>
2025-01-12 09:14:21 -0500

    edits to main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Footer from './Components/Footer/Footer.jsx'
-import Landing from './Components/Landing/Landing.jsx'
+import Landing from './Components/Landing/Landing.Jsx'
 import NavBar from './Components/NavBar/Nav'
 import About from './Components/About/About.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -17,7 +17,7 @@ const Landing = () => {
               playsInline
               className="absolute top-0 left-0 w-full h-full object-cover z-0 brightness-75"
             >
-              <source src="/back2.mp4" type="video/mp4" />
+              <source src="/jaxVid.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
 
